624977820537814ff1890561dd2b934f6a527f01
bjhufstetler <bjhufstetler@gmail.com>
2019-09-29 08:05:51 -0400

    cleaned code

--- Data_Work.R
+++ Data_Work.R
@@ -34,4 +34,42 @@ plot(count.missing2)
 complete <- complete.cases(data)
 data <- data[complete,]
 
-write.csv(data, file="data_complete.csv")
+#write.csv(data, file="data_complete.csv")
+
+# Naive Bayes does not work because it needs categorical information
+
+# PCA
+#install.packages("factoextra")
+library(factoextra)
+
+PCAdata <- data
+prin_comp <- prcomp(data[-c(2:3)], scale.=T)
+biplot(prin_comp, scale=0)
+
+std_dev <- prin_comp$sdev
+pr_var <- std_dev^2
+pr_varex <- pr_var/sum(pr_var)
+
+plot(pr_varex, xlab = "Principal Component", ylab = "Proportion of VAriance Explained", type = "b")
+plot(cumsum(pr_varex), xlab="Principal Component", ylab="Cumulative Proportion of Variance Explained", type="b")
+
+#fviz_eig(prin_comp)
+#fviz_pca_ind(prin_comp, col.ind = "cos2", gradient.cols = c("#00AFBB","#E7B800","#FC4E07"), repel=TRUE)
+
+factanal(PCAdata,2,rotation='varimax')
+
+
+#k means clustering
+for (i in 4:880){
+data.norm[,(i-3)] <- (data[,i]-mean(data[,i]))/sd(data[,i])
+}
+dist_data <- dist(data[-c(1:3)])
+hc_data <- hclust(dist_data, method="complete")
+clusters_k4 <- cutree(hc_data, k=2)
+
+library(dendextend)
+dend_data <- as.dendrogram(hc_data)
+dend_colored <- color_branches(dend_data, h=6e16)
+plot(dend_colored)
+
+clusters <- kmeans(data[-c(1:3),],3)
